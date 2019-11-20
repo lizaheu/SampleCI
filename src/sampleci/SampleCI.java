8953e177a73138f10a70a8a8d96d43b84502b976
taruc <taruc@B25>
2019-11-20 10:40:41 +0800

    update

--- src/sampleci/SampleCI.java
+++ src/sampleci/SampleCI.java
@@ -5,6 +5,7 @@ public class SampleCI {
     public static void main(String[] args) {
         System.out.println("Happy");
         System.out.println("Welcome");
+        System.out.println("Welcome");
     }
     
 }

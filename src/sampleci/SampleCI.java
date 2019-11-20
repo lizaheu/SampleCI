704aa1301bf2997d80f2a9acf1684b7cbf6d6df3
taruc <taruc@B25>
2019-11-20 10:25:10 +0800

    initial

---
+++ src/sampleci/SampleCI.java
@@ -0,0 +1,10 @@
+package sampleci;
+
+public class SampleCI {
+
+    public static void main(String[] args) {
+        System.out.println("Happy");
+        System.out.println("Welcome");
+    }
+    
+}
